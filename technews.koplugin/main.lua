2efb14a9c44ad03f7753982cb91d1f07267cf8a3
isword <isword@qq.com>
2026-09-24 12:40:43 +0800

    chore: bump version to 0.1.9

--- technews.koplugin/main.lua
+++ technews.koplugin/main.lua
@@ -43,7 +43,7 @@ local MAX_IMAGES_PER_ISSUE = 150
 local TechNews = WidgetContainer:extend{
     name = "technews",
     is_doc_only = false,
-    version = "0.1.8",
+    version = "0.1.9",
 }
 
 -- 自测只执行一次：插件用 dofile 加载，模块级变量会随 UI 重建被重置，
